c06d2a7eb7194fdb166c3277cca0a0e8d671c04a
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-01-27 12:50:45 -0600

    styling for search

--- src/components/home-components/hero.tsx
+++ src/components/home-components/hero.tsx
@@ -5,7 +5,7 @@ import SearchContainer from './searchContainer';
 
 export default function Hero() {
   return (
-<div className="relative h-[70vh] flex items-center justify-center text-white">
+    <div className="relative h-[70vh] flex items-center justify-center text-white">
       {/* Background image */}
       <Image
         src="/paul-weaver-hero.jpg" // Replace with the path to your image
@@ -18,12 +18,8 @@ export default function Hero() {
       {/* Overlay */}
       <div className="absolute bg-black bg-opacity-50 inset-0 z-10" />
       {/* Text */}
-      <div className="relative z-20 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-5xl font-bold">Welcome to Our Website</h1>
-        <p className="mt-6 text-xl">
-          Discover the most amazing experiences all around the world.
-        </p>
-        <SearchContainer  />
+      <div className="relative translate-y-[-160%] z-20 w-[40vw]">
+        <SearchContainer />
       </div>
     </div>
   )
--- src/components/home-components/searchContainer.tsx
+++ src/components/home-components/searchContainer.tsx
@@ -4,20 +4,20 @@ import { BiSearch } from 'react-icons/bi'
 
 export default function SearchContainer() {
   return (
-    <div className="border border-gray-500 w-full md:w-auto py-2 px-4 rounded-full bg-white text-gray-500   shadow-sm hover:shadow-md transition cursor-pointer">
+    <div className="border border-gray-500 w-full md:w-auto py-4 px-4 rounded-full bg-white text-gray-500   shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="flex flex-row items-center justify-between">
 
-        <div className="text-sm font-semibold px-6">
+        <div className="text-sm  pl-2 pr-8">
           Where to?
         </div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] border-gray-500 flex-1 text-center">
+        <div className="hidden sm:block text-sm  px-6 border-x-[1px] border-gray-500 flex-1">
           Move In:
         </div>
-        <div className="hidden sm:block text-sm font-semibold px-6 border-r-[1px] border-gray-500 flex-1 text-center">
+        <div className="hidden text-left sm:block text-sm px-6 border-r-[1px] border-gray-500 flex-1 ">
           Move Out:
         </div>
-        <div className="text-sm pl-6 pr-2  flex flex-row items-center gap-3">
-          Who?
+        <div className="text-sm pl-2 pr-8  ">
+         <p>Who?</p> 
         </div>
         <div className="p-2 bg-primary rounded-full text-white ">
           <BiSearch />
