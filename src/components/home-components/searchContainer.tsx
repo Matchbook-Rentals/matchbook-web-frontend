c48b5de5287a3a4087d676d62a6a8c2a76b0dd6d
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-01-27 12:56:45 -0600

    text sizes and location adjusted

--- src/components/home-components/searchContainer.tsx
+++ src/components/home-components/searchContainer.tsx
@@ -7,19 +7,19 @@ export default function SearchContainer() {
     <div className="border border-gray-500 w-full md:w-auto py-4 px-4 rounded-full bg-white text-gray-500   shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="flex flex-row items-center justify-between">
 
-        <div className="text-sm  pl-2 pr-8">
+        <div className="text-lg  pl-2 pr-8">
           Where to?
         </div>
-        <div className="hidden sm:block text-sm  px-6 border-x-[1px] border-gray-500 flex-1">
+        <div className="hidden sm:block text-lg  px-6 border-x-[1px] border-gray-500 flex-1">
           Move In:
         </div>
-        <div className="hidden text-left sm:block text-sm px-6 border-r-[1px] border-gray-500 flex-1 ">
+        <div className="hidden text-left sm:block text-lg px-6 border-r-[1px] border-gray-500 flex-1 ">
           Move Out:
         </div>
-        <div className="text-sm pl-2 pr-8  ">
+        <div className="text-lg pl-2 pr-8  ">
          <p>Who?</p> 
         </div>
-        <div className="p-2 bg-primary rounded-full text-white ">
+        <div className="p-2 text-lg bg-primary rounded-full text-white ">
           <BiSearch />
         </div>
       </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
       <nav className="bg-primary flex justify-between py-3 w-full ">
         <Image src={"/logo-nav-full.png"} alt="logo" width={400} height={400} />
         <div className="flex items-center gap-6 pr-6">
-          <button className="rounded-full bg-white text-lg border border-gray-500 px-5 py-3 text-gray-500 text-center ">List Your Place</button>
+          <button className="rounded-full xl:mr-10 bg-white text-lg border border-gray-500 px-5 py-3 text-gray-500 text-center ">List Your Place</button>
           <UserMenu />
         </div>
       </nav>
