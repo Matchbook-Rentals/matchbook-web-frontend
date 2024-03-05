36165a29d82c84e3290487577e98dd6c2c756252
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-03-05 01:34:19 -0600

    skeleton for dashboard

--- src/app/platform/dashboard/page.tsx
+++ src/app/platform/dashboard/page.tsx
@@ -18,7 +18,14 @@ export default async function DashboardPage
   console.log(trips);
   return (
     <div>
-      Hello
+      <h2 className='text-center text-3xl border-b-2 pb-3'>Your Current Booking</h2>
+      <h2 className='text-center text-3xl border-b-2 pb-3'>Your Reserved Bookings </h2>
+      <h2 className='text-center text-3xl border-b-2 pb-3'>Your Recent Searches </h2>
+      {trips.map((trip, idx) => (
+        <p key={trip.id}>
+          {trip.id}
+        </p>
+      ))}
 
     </div>
   )
