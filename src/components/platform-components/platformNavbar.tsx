a1ea8b06c29dc0fe2f6cea1b490317d58adf6613
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-03-05 01:05:27 -0600

    navbar redesign implemented

--- src/components/platform-components/platformNavbar.tsx
+++ src/components/platform-components/platformNavbar.tsx
@@ -7,10 +7,15 @@ export default async function PlatformNavbar() {
   const user = await currentUser();
   let isSignedIn = Boolean(user);
   return (
-    <nav className="bg-white flex items-center justify-between py-2 px-9 w-full ">
-      <div></div>
-      <Image src={"/logo-nav-full.png"} alt="logo" width={450} height={450} className='' />
-      <div className="justify-self-right">
+    <nav className="bg-white flex items-center justify-between pt-2 pr-9 w-full border-b border-black mb-3 ">
+      <div className='w-1/3'>
+        <Image src={"/logo-nav-2.png"} alt="logo" width={400} height={400} className='' />
+      </div>
+      <div className='w-1/3 flex justify-center translate-y-8'>
+        <Image src={"/heart-logo.png"} alt="logo" width={60} height={60} className='' />
+
+      </div>
+      <div className="w-1/3 flex justify-end">
         <UserMenu color='black' isSignedIn={isSignedIn} />
       </div>
     </nav>
