85fa6369f02bb39f041692f395014dc35a7439c0
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-03-05 19:38:08 -0600

    fix issue caused by caching old db results

--- src/app/platform/dashboard/recent-searches.tsx
+++ src/app/platform/dashboard/recent-searches.tsx
@@ -8,7 +8,7 @@ export default function RecentSearches({trips}: {trips: Trip[]}) {
       <h2 className='text-center w-1/2 mx-auto mb-5 text-3xl border-b-2 pb-3 font-semibold'>Your Recent Searches</h2>
       {/* THis needs to be changed to === instead of !== once db is updated with tripStatus */}
       {trips.map((trip, idx) => {
-        if (trip.tripStatus !== 'searching') return <TripBar key={trip.id} trip={trip} />
+        if (trip.tripStatus === 'searching') return <TripBar key={trip.id} trip={trip} />
       })}
     </div>
   )
