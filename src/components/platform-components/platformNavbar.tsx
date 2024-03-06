d7e6582b38090f30898708872e5814893c2912f7
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-03-05 19:27:28 -0600

    navbar link fix

--- src/components/platform-components/platformNavbar.tsx
+++ src/components/platform-components/platformNavbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import UserMenu from '../userMenu'
 import { currentUser } from '@clerk/nextjs'
+import Link from 'next/link';
 
 export default async function PlatformNavbar() {
   const user = await currentUser();
@@ -9,7 +10,9 @@ export default async function PlatformNavbar() {
   return (
     <nav className="bg-white flex items-center justify-between pt-2 pr-9 w-full border-b sticky top-0 border-black mb-9 ">
       <div className='w-1/3'>
-        <Image src={"/logo-nav-2.png"} alt="logo" width={400} height={400} className='' />
+        <Link href={'/'}>
+          <Image src={"/logo-nav-2.png"} alt="logo" width={400} height={400} className='' />
+        </Link>
       </div>
       <div className='w-1/3 flex justify-center translate-y-8'>
         <Image src={"/heart-logo.png"} alt="logo" width={60} height={60} className='' />
