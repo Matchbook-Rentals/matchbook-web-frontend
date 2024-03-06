db39eba3702b0b1ce69665837b66c25bb2d7bcdc
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-03-05 18:31:42 -0600

    sticky top nav, needs transparent heart logo, trip bar style tweaks

--- src/app/platform/dashboard/trip-bar.tsx
+++ src/app/platform/dashboard/trip-bar.tsx
@@ -12,33 +12,33 @@ export default function TripBar({ trip }: { trip: Trip }) {
   }
   return (
     <div onClick={handleClick} className='flex justify-between border-2 border-slate-400 w-3/4 mx-auto my-3 p-2 rounded-lg shadow-lg text-2xl cursor-pointer'>
-      <div>
+      <div className='w-1/4'>
         <span className='mr-3'>&#x1F4CD;</span>
         {/* {trip.city}, {trip.state} */}
         {trip.locationString ? trip.locationString : 'Somewhere special'}
       </div>
-      <div>
+      <div className='w-1/4'>
         <span className='mr-3'>&#x1F4C5;</span>
         {
           trip.startDate ?
             trip.startDate.toLocaleDateString('en-GB', {
-              day: '2-digit', month: 'long', year: 'numeric'
+              day: '2-digit', month: 'short', year: '2-digit'
             }) :
             'Flexible'
         } - {
           trip.endDate ?
             trip.endDate.toLocaleDateString('en-GB', {
-              day: '2-digit', month: 'long', year: 'numeric'
+              day: '2-digit', month: 'short', year: '2-digit'
             }) :
             'Flexible'
         }
 
       </div>
-      <div>
+      <div className=''>
         <span className='mr-3'>&#x1F464;</span>
-        {trip.numAdults && trip.numAdults} adults, {trip.numChildren && trip.numChildren} children, {trip.numPets && trip.numPets} pets
+        {trip.numAdults && trip.numAdults} adults, {trip.numChildren && trip.numChildren} child, {trip.numPets && trip.numPets} pets
       </div>
-      <div>
+      <div className='w-1/6'>
         <span className='mr-3'>&#x1F58A;</span>
         {trip.tripStatus ? trip.tripStatus : ' Searching'}
       </div>
--- src/components/platform-components/platformNavbar.tsx
+++ src/components/platform-components/platformNavbar.tsx
@@ -7,7 +7,7 @@ export default async function PlatformNavbar() {
   const user = await currentUser();
   let isSignedIn = Boolean(user);
   return (
-    <nav className="bg-white flex items-center justify-between pt-2 pr-9 w-full border-b border-black mb-9 ">
+    <nav className="bg-white flex items-center justify-between pt-2 pr-9 w-full border-b sticky top-0 border-black mb-9 ">
       <div className='w-1/3'>
         <Image src={"/logo-nav-2.png"} alt="logo" width={400} height={400} className='' />
       </div>
