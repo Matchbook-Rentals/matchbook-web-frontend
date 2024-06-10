06466c87792697571a5b94186582f537500eac09
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-06-10 04:21:37 -0500

    pull user from server side clerk fx

--- src/app/platform/host-dashboard/add-property/page.tsx
+++ src/app/platform/host-dashboard/add-property/page.tsx
@@ -3,17 +3,39 @@ import prisma from "@/lib/prismadb";
 import ProgressBar from "./progress-bar";
 import AddPropertyClient from "./app-property-client";
 import { type Listing } from "@prisma/client";
+import { currentUser } from "@clerk/nextjs/server";
 
 const handleListingCreation = async (propertyDetails: Listing) => {
   "use server";
 
-  const listing = await prisma.listing.create({
-    data: {
-      ...propertyDetails,
-    },
-  });
-  console.log(listing);
-  return listing;
+  try {
+    const user = await currentUser();
+    if (!user) {
+      throw new Error("User not authenticated");
+    }
+    // Fetch user data from Clerk
+
+    const listing = await prisma.listing.create({
+      data: {
+        ...propertyDetails,
+        userId: user.id, // Associate the listing with the user
+      },
+    });
+    console.log('TRUE', listing);
+
+    // Temporary code to automatically delete the created listing
+    await prisma.listing.delete({
+      where: {
+        id: listing.id,
+      },
+    });
+    console.log('Listing deleted:', listing.id);
+
+    return 'true';
+  } catch (error: any) {
+    return error.message;
+    throw error; // Re-throw the error if you want to propagate it further
+  }
 };
 
 export default function AddPropertyPage() {
