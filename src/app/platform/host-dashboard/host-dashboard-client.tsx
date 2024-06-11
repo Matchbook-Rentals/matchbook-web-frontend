13c5f07b8ae7df5f45e5753e055367195dc738eb
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-06-11 03:52:50 -0500

    move host-dashboard away from mocked data

---
+++ src/app/platform/host-dashboard/host-dashboard-client.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+import HostDashboardHeader from './host-dashboard-header';
+import PropertyList from './property-list';
+import { Listing, ListingImage } from '@prisma/client';
+
+interface HostDashboardClientProps {
+  properties: Listing & { listingImages: ListingImage[] }[];
+}
+
+const HostDashboardClient: React.FC<HostDashboardClientProps> = ({ properties }) => {
+  const [statusFilter, setStatusFilter] = React.useState<string | null>(null);
+  const [filteredProperties, setFilteredProperties] = React.useState<Listing & { listingImages: ListingImage[] }[]>(properties);
+
+
+  React.useEffect(() => {
+    if (statusFilter) {
+      setFilteredProperties(properties.filter(property => property.status === statusFilter));
+    } else {
+      setFilteredProperties(properties);
+    }
+  }, [statusFilter, properties]);
+
+  return (
+    <>
+      <HostDashboardHeader properties={properties} statusFilter={statusFilter} setStatusFilter={setStatusFilter} />
+      <PropertyList properties={filteredProperties} filter={statusFilter} />
+    </>
+  );
+};
+
+export default HostDashboardClient;
+
