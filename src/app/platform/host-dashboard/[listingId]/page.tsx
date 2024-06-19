387c8801e58825acc31147d857f3c26b6c719de8
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-06-19 00:55:18 -0500

    move to custom svgs

--- src/app/platform/host-dashboard/[listingId]/page.tsx
+++ src/app/platform/host-dashboard/[listingId]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ListingHorizontalCard from '@/components/ui/listing-horizontal-card';
 import TabSelector from '@/components/ui/tab-selector';
 import { useHostProperties } from '../../../../contexts/host-properties-provider';
-import { useRouter } from 'next/navigation';
+import { OverviewIcon, ListingIcon, ApplicationsIcon, PaymentsIcon, BookingsIcon, AnalyticsIcon } from '@/components/svgs/svg-components';
 
 const PropertyDetails: React.FC = ({ params }) => {
   const { listings } = useHostProperties();
@@ -15,10 +15,20 @@ const PropertyDetails: React.FC = ({ params }) => {
     return <div>Property not found</div>;
   }
 
+
+  const tabs = [
+    { value: "overview", label: "Overview", icon: OverviewIcon, content: <div>Overview content goes here.</div> },
+    { value: "listing", label: "Listing", icon: ListingIcon, content: <div>Listing content goes here.</div> },
+    { value: "applications", label: "Applications", icon: ApplicationsIcon, content: <div>Applications content goes here.</div> },
+    { value: "payments", label: "Payments", icon: PaymentsIcon, content: <div>Payments content goes here.</div> },
+    { value: "analytics", label: "Analytics", icon: AnalyticsIcon, content: <div>Analytics content goes here.</div> },
+    { value: "calendar", label: "Calendar", icon: BookingsIcon, content: <div>Calendar content goes here.</div> },
+  ]
+
   return (
     <div className='px-4'>
       <ListingHorizontalCard imgSrc={listing.listingImages[0].url} title={listing.title} status={listing.status} address={listing.locationString} />
-      <TabSelector tabs={['Description', 'Amenities', 'Photos', 'Availability', 'Price', 'Calendar']} />
+      <TabSelector tabs={tabs} />
       {/* Add more details as needed */}
     </div>
   );
--- src/components/ui/listing-horizontal-card.tsx
+++ src/components/ui/listing-horizontal-card.tsx
@@ -9,13 +9,13 @@ interface ListingHorizontalCardProps {
 
 export default function ListingHorizontalCard({ imgSrc, title, status, address }: ListingHorizontalCardProps) {
   return (
-    <div className="flex max-w-[750px] max-h-[200px] border-2 border-gray-500 rounded-md overflow-hidden">
+    <div className="flex max-w-[750px] max-h-[150px] border-2 border-gray-300 rounded-md overflow-hidden">
       <img src={imgSrc} alt="House" className="w-1/2 object-cover" />
       <div className="flex flex-col justify-center p-4 w-1/2">
         <h2 className="text-2xl font-bold">{title}</h2>
-        <p className="mt-2">{address}</p>
-        <div className="mt-4">
-          <Badge className="bg-gray-200 text-gray-800 px-4 py-2">{status}</Badge>
+        <p className="mt-2 text-center">{address}</p>
+        <div className="mt-4 flex justify-end">
+          <Badge className="bg-primaryBrand text-black text-md font-bold rounded-none px-2 py-1">{status.toUpperCase()}</Badge>
         </div>
       </div>
     </div>
