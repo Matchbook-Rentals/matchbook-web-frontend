6dc2d2e8f44421ab3a995825e4e92f19f90d2c5b
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-06-19 01:07:48 -0500

    framer-motion underline

--- src/components/ui/tab-selector.tsx
+++ src/components/ui/tab-selector.tsx
@@ -1,5 +1,7 @@
 'use client'
+import { useState } from "react"
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
+import { motion } from "framer-motion";
 
 interface Tab {
   value: string;
@@ -13,16 +15,21 @@ interface TabSelectorProps {
 }
 
 export default function TabSelector({ tabs }: TabSelectorProps) {
+  const [activeTab, setActiveTab] = useState(tabs[0]?.value);
+
   return (
     <div className="flex border-2 border-red-500 justify-start space-x-2 py-4 border-b">
       <Tabs defaultValue={tabs[0]?.value}>
-        <TabsList className="flex mb-4 py-6 bg-primaryBrand border-b-2 border-gray-300 space-x-2">
+        <TabsList className="flex mb-4 pt-6 pb-8 border-b-2 border-gray-300 space-x-2">
           {tabs.map((tab) => (
-            <TabsTrigger key={tab.value} value={tab.value} className="flex flex-col items-center hover:bg-gray-300">
+            <TabsTrigger key={tab.value} value={tab.value} className="flex flex-col items-center hover:bg-gray-300" onClick={() => setActiveTab(tab.value)}>
               <div className="h-8 w-8 flex items-center justify-center">
                 <tab.icon className="h-2 w-2 text-sm" />
               </div>
-              <span className="text-sm">{tab.label}</span>
+              <div className="flex flex-col">
+                <span className="text-sm">{tab.label}</span>
+                {activeTab === tab.value && <motion.div className="h-1 w-full bg-black rounded-full" layout layoutId="underline"></motion.div>}
+              </div>
             </TabsTrigger>
           ))}
         </TabsList>
