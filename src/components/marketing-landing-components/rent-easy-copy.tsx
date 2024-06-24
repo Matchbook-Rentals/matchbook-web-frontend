db536e651b6f2762fe6dfef6cc5017b150f8663a
TYLER BENNETT <tyler.bennett52@gmail.com>
2024-06-24 08:36:22 -0500

    change whitespace for easy copy

--- src/components/marketing-landing-components/rent-easy-copy.tsx
+++ src/components/marketing-landing-components/rent-easy-copy.tsx
@@ -1,10 +1,10 @@
 export default function RentEasyCopy() {
   let blackLineStyle = "w-1/5 h-[2px] bg-black"
   return (
-    <div className="flex w-full items-center justify-center py-8">
+    <div className="flex w-full my-10 items-center justify-center py-8">
       <div className="flex w-[90%] space-x-4 items-center justify-between">
         <div className={blackLineStyle} />
-        <div className="text-left pl-3  w-3/5  text-5xl font-semibold text-gray-800">
+        <div className="text-center pl-3  w-3/5  text-5xl font-semibold text-gray-800">
           <p>Renting shouldn&apos;t be so hard.</p>
           <p>Now, it doesn&apos;t have to be.</p>
         </div>
