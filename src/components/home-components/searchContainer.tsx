8922dfbff25e70d7c9bdfa9874e0188dfe3f5396
Tyler Bennett <tyler.bennett52@gmail.com>
2024-07-02 17:20:47 +0000

    fix merge error

--- src/components/home-components/searchContainer.tsx
+++ src/components/home-components/searchContainer.tsx
@@ -120,42 +120,4 @@ export default function SearchContainer({ createTrip }: SearchContainerProps) {
     console.log(tripDetails);
     router.push('/platform/preferences');
   }
-
-const SearchContainer: React.FC<SearchContainerProps> = ({ className }) => {
-
-  return (
-    <div className={`mx-auto p-2 ${className || ''}`}>
-      <div className="flex p-3 items-center bg-white rounded-full shadow-md overflow-hidden">
-        <input
-          type="text"
-          placeholder="Where to?"
-          disabled
-          className="w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none disabled:bg-transparent cursor-not-allowed border-r border-gray-300"
-        />
-        <input
-          type="text"
-          placeholder="Move in:"
-          disabled
-          className="w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none disabled:bg-transparent cursor-not-allowed border-r border-gray-300"
-        />
-        <input
-          type="text"
-          placeholder="Move out:"
-          disabled
-          className="w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none disabled:bg-transparent cursor-not-allowed border-r border-gray-300"
-        />
-        <input
-          type="text"
-          placeholder="Who?"
-          disabled
-          className="w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none disabled:bg-transparent cursor-not-allowed"
-        />
-        <button className="p-3 cursor-not-allowed bg-primaryBrand rounded-full">
-          <FaSearch className="text-white" size={20} />
-        </button>
-      </div>
-    </div>
-  );
-};
-
-export default SearchContainer;
+}
