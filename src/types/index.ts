d183aed4164d48bd3567afe1238fb92a3151104d
Tyler Bennett <tyler.bennett52@gmail.com>
2024-07-08 16:26:33 +0000

    add dislikes to trip type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
-import { HousingRequest, ListingImage, Match, Favorite, Trip, User, Listing } from "@prisma/client";
+import { HousingRequest, Dislike, ListingImage, Match, Favorite, Trip, User, Listing } from "@prisma/client";
 
 export interface TripAndMatches extends Trip {
   favorites: Favorite[]
   matches: Match[]
+  housingRequests: HousingRequest[]
+  dislikes: Dislike[]
 };
 
 export interface ListingAndImages extends Listing {
