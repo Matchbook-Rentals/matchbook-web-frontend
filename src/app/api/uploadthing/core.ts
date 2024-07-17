b7479a9da68974161cc92c3374569bee06b379e4
Tyler Bennett <tyler.bennett52@gmail.com>
2024-07-17 10:29:23 +0000

    image upload for ids

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -42,7 +42,28 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
+      console.log("Proof of income upload for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+
+      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { uploadedBy: metadata.userId, fileUrl: file.url };
+    }),
+  idUploader: f({ image: { maxFileSize: "8MB", maxFileCount: 8 } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = auth();
+
+      // If you throw, the user will not be able to upload
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("ID image upload for userId:", metadata.userId);
 
       console.log("file url", file.url);
 
