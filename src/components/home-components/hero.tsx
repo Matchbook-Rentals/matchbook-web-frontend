02004ebeb615f7d8ab7834088e075d30c531fc27
Tyler Bennett <tyler.bennett52@gmail.com>
2024-10-15 12:24:37 -0500

    change hero image crop direction

--- src/components/home-components/hero.tsx
+++ src/components/home-components/hero.tsx
@@ -4,13 +4,19 @@ import Countdown from "../marketing-landing-components/countdown";
 
 const Hero: React.FC = () => {
   return (
-    <div className="relative min-h-[60vh] w-full md:w-[90vw] lg:w-[80vw] mx-auto flex flex-col items-center px-4 sm:px-8 md:px-12 bg-cover justify-start" style={{ backgroundImage: "url('/hero-image.png')" }}>
-
+    <div
+      className="relative min-h-[60vh] w-full md:w-[90vw] lg:w-[80vw] mx-auto flex flex-col items-center px-4 sm:px-8 md:px-12 bg-cover justify-start"
+      style={{
+        backgroundImage: "url('/hero-image.png')",
+        backgroundSize: "cover", // Ensures the image covers the container
+        backgroundPosition: "center", // Centers the image so it crops equally from all sides
+      }}
+    >
       {/* Overlay */}
       {/* <div className="absolute inset-0 bg-gray-400 opacity-50"></div> */}
 
       {/* Content */}
-      <SearchContainer className="md:pt-[5%] pt-[15%] sm:w-[90%] md:w-[70%] relative" />
+      <SearchContainer className="md:pt-[5%] pt-[5%] sm:w-[90%] md:w-[70%] relative" />
     </div>
   );
 };
