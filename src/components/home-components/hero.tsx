ef316402b35f8096e176b2d7869171a989c10a29
Tyler Bennett <tyler.bennett52@gmail.com>
2024-10-15 07:48:16 -0500

    change hero image, add test classes

--- src/components/home-components/hero.tsx
+++ src/components/home-components/hero.tsx
@@ -4,7 +4,7 @@ import Countdown from "../marketing-landing-components/countdown";
 
 const Hero: React.FC = () => {
   return (
-    <div className="relative h-[60vh] w-[80vw] mx-auto flex flex-col items-center px-12 bg-cover justify-start" style={{ backgroundImage: "url('/temp-header-cozy-couch.png')" }}>
+    <div className="relative  mx-auto flex flex-col items-center px-12 bg-cover justify-start" style={{ backgroundImage: "url('/hero-image.png')" }}>
       {/* Overlay */}
       <div className="absolute inset-0 bg-gray-400 opacity-50"></div>
 
