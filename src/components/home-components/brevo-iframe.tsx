a137d26146b03bcab4a413ead336f286e35b9053
Tyler Bennett <tyler.bennett52@gmail.com>
2024-10-16 21:46:24 -0500

    style fix

--- src/components/home-components/brevo-iframe.tsx
+++ src/components/home-components/brevo-iframe.tsx
@@ -2,21 +2,15 @@
 
 import React from "react";
 
-const BrevoIframe = () => {
+const BrevoIframe: React.FC = () => {
   return (
     <iframe
-      width="540"
-      height="305"
       src="https://2ad770f1.sibforms.com/serve/MUIFAHcHg4cAoKga0ovVndvaqvTLm79G59UuyemMwyOJeTyPhUNp-g6LmJbH4rrhKx7eJbwIbJvtltohK9SDGDwSl0frYWavjdD1nDXykWCYRl2QncBtCCxkYkxW5Z2TTIMR1R1BEKkuRgIVbHRwzmPUq0fsvydxbDT7RH90JklhBcHDRnTnRUWt-ae2ED9iofvTHC0D9WEJa5cc"
-      frameBorder="0"
-      scrolling="auto"
-      allowFullScreen
-      style={{
-        display: "block",
-        marginLeft: "auto",
-        marginRight: "auto",
-        maxWidth: "100%"
-      }}
+      width="100%"
+      height="100%"
+      style={{ border: "none", margin: "0 auto", overflow: "hidden" }}
+      title="Brevo Form"
+      scrolling="no"
     />
   );
 };
