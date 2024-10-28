f100624af83097f3e8628d72718b5923ef7c573d
Tyler Bennett <tyler.bennett52@gmail.com>
2024-10-28 06:59:41 -0500

    add whitespace between tabList and tab display area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,7 +188,7 @@ const WebHomePage = () => {
         useUrlParams
         tabs={tabs}
         className="max-w-[800px] px-8 mx-auto mt-12 xs:mt-32 border-b-0"
-        tabsListClassName="justify-center w-full border-b-0 mx-auto"
+        tabsListClassName="justify-center w-full border-b-0 mx-auto mb-12"
       />
       <div className="justify-center flex border-b-0 mx-auto w-1/2 p-6">
         <Button className={`bg-blueBrand px-16 mt-2 py-2 text-background text-xl rounded-sm ${montserrat.className}`} onClick={() => setIsOpen(true)}> Get Notified </Button>
