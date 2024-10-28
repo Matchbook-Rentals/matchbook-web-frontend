24fe0b97cfef157f6d7fbd271d06f450eb15b224
Tyler Bennett <tyler.bennett52@gmail.com>
2024-10-28 10:21:13 -0500

    remove focus ring

--- src/components/home-components/brevo-iframe.tsx
+++ src/components/home-components/brevo-iframe.tsx
@@ -2,11 +2,20 @@
 
 import React from "react";
 
+const BREVO_FORM_URL =
+  "https://2ad770f1.sibforms.com/serve/" +
+  "MUIFAHcHg4cAoKga0ovVndvaqvTLm79G59UuyemMwyOJeTyPhUNp-g6LmJbH4rrhKx7eJbw" +
+  "IbJvtltohK9SDGDwSl0frYWavjdD1nDXykWCYRl2QncBtCCxkYkxW5Z2TTIMR1R1BEKkuRg" +
+  "IVbHRwzmPUq0fsvydxbDT7RH90JklhBcHDRnTnRUWt-ae2ED9iofvTHC0D9WEJa5cc";
+
 const BrevoIframe: React.FC = () => {
   return (
     <iframe
-      src="https://2ad770f1.sibforms.com/serve/MUIFAHcHg4cAoKga0ovVndvaqvTLm79G59UuyemMwyOJeTyPhUNp-g6LmJbH4rrhKx7eJbwIbJvtltohK9SDGDwSl0frYWavjdD1nDXykWCYRl2QncBtCCxkYkxW5Z2TTIMR1R1BEKkuRgIVbHRwzmPUq0fsvydxbDT7RH90JklhBcHDRnTnRUWt-ae2ED9iofvTHC0D9WEJa5cc"
-      className="border-0 w-full h-[750px] sm:h-[600px] overflow-y-scroll mt-[-13%] "
+      src={BREVO_FORM_URL}
+      className={
+        "border-0 border-white focus:ring-0 focus:outline-none " +
+        "w-full h-[750px] sm:h-[600px] overflow-y-scroll mt-[-13%]"
+      }
       title="Brevo Form"
       scrolling="no"
     />
