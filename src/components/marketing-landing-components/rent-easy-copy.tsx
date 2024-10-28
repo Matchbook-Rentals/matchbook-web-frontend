42d4c34c1de6d0138f5bd1c84283e3e263111303
Tyler Bennett <tyler.bennett52@gmail.com>
2024-10-28 08:11:26 -0500

    reorganize components and whitespace

--- src/components/marketing-landing-components/rent-easy-copy.tsx
+++ src/components/marketing-landing-components/rent-easy-copy.tsx
@@ -1,7 +1,7 @@
 export default function RentEasyCopy() {
   let blackLineStyle = "w-1/5 h-[2px] bg-black hidden sm:block";
   return (
-    <div className="flex w-full my-5 sm:my-10 items-center justify-center py-4 sm:py-8">
+    <div className="flex w-full mt-72 mb-36 items-center justify-center py-4 sm:py-8">
       <div className="flex w-full sm:w-[90%] px-4 sm:px-0 space-x-0 sm:space-x-4 items-center justify-between">
         <div className={blackLineStyle} />
         <div className="text-center w-full sm:w-3/5 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-800">
