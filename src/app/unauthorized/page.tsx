193b3b7a3efd43e452bd91be8a684ce57cd4d523
Tyler Bennett <tyler.bennett52@gmail.com>
2024-11-24 12:57:47 -0600

    entity fix

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -3,7 +3,7 @@ export default function Unauthorized() {
   return (
     <div>
       <h1>Access Denied</h1>
-      <p>You don't have permission to access this resource.</p>
+      <p>You don&apos;t have permission to access this resource.</p>
     </div>
   );
 }
