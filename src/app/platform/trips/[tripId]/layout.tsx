1e189853dd94bc6641d088d47ed28a4c6e5d24f4
Tyler Bennett <tyler.bennett52@gmail.com>
2024-12-02 23:52:34 -0600

    implement suspense for listings fetch delay

--- src/app/platform/trips/[tripId]/layout.tsx
+++ src/app/platform/trips/[tripId]/layout.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import { TripContextProvider } from '@/contexts/trip-context-provider';
-
+import LoadingSkeleton from './LoadingSkeleton';
 import { pullListingsFromDb } from '@/app/actions/listings';
 import { getTripById } from '@/app/actions/trips';
 import { getUserApplication } from '@/app/actions/applications';
-import LoadingSkeleton from './LoadingSkeleton';
 
-export default async function TripLayout({ children, params }: { children: React.ReactNode, params: { tripId: string } }) {
+async function TripDataWrapper({ children, params }: {
+  children: React.ReactNode,
+  params: { tripId: string }
+}) {
   const trip = await getTripById(params.tripId);
   if (!trip) { return <p> NO TRIP FOUND </p> }
 
   const listings = await pullListingsFromDb(trip.latitude, trip.longitude, 100);
   const application = await getUserApplication();
   const hasApplicationData = !!application;
-
-  if (!trip || !listings || !application) {
-    return <LoadingSkeleton />;
-  }
 
   return (
     <TripContextProvider
@@ -27,5 +25,21 @@ export default async function TripLayout({ children, params }: { children: React
     >
       {children}
     </TripContextProvider>
+  );
+}
+
+export default function TripLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode,
+  params: { tripId: string }
+}) {
+  return (
+    <React.Suspense fallback={<LoadingSkeleton />}>
+      <TripDataWrapper params={params}>
+        {children}
+      </TripDataWrapper>
+    </React.Suspense>
   );
 }
