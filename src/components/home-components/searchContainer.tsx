cece5cbc1d3c223734be77d4dcaeadb1111784d5
Tyler Bennett <tyler.bennett52@gmail.com>
2024-12-03 18:38:38 -0600

    split by size

--- src/components/home-components/searchContainer.tsx
+++ src/components/home-components/searchContainer.tsx
@@ -1,69 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { FaSearch } from "react-icons/fa";
-import { useAuth, useUser } from "@clerk/nextjs";
 
 interface SearchContainerProps {
   className?: string;
 }
 
 const SearchContainer: React.FC<SearchContainerProps> = ({ className }) => {
-  const [hasAccess, setHasAccess] = useState(false);
-  const { isSignedIn } = useAuth();
-  const { user } = useUser();
-
-  useEffect(() => {
-    const checkAccess = async () => {
-      if (isSignedIn && user) {
-        const userRole = user.publicMetadata.role as string;
-        setHasAccess(userRole === 'moderator' || userRole === 'admin' || userRole === 'beta_user');
-      }
-    };
-
-    checkAccess();
-  }, [isSignedIn, user]);
-
   return (
-    <div className={`mx-auto p-2 ${className || ""}`}>
-      <div className="relative">
-        <div className="flex flex-col sm:flex-row p-3 items-center bg-gray-100 rounded-3xl sm:rounded-full shadow-md overflow-hidden">
-          <input
-            type="text"
-            placeholder="Where to?"
-            className={`w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 bg-transparent
-              ${!hasAccess ? 'cursor-not-allowed opacity-50' : ''}`}
-            readOnly={!hasAccess}
-          />
-          <input
-            type="text"
-            placeholder="Move in:"
-            className={`w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 bg-transparent
-              ${!hasAccess ? 'cursor-not-allowed opacity-50' : ''}`}
-            readOnly={!hasAccess}
-          />
-          <input
-            type="text"
-            placeholder="Move out:"
-            className={`w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 bg-transparent
-              ${!hasAccess ? 'cursor-not-allowed opacity-50' : ''}`}
-            readOnly={!hasAccess}
-          />
-          <input
-            type="text"
-            placeholder="Who?"
-            className={`w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none bg-transparent
-              ${!hasAccess ? 'cursor-not-allowed opacity-50' : ''}`}
-            readOnly={!hasAccess}
-          />
-          <button
-            disabled={!hasAccess}
-            className={`w-full sm:w-auto mt-3 sm:mt-0 p-3 bg-primaryBrand rounded-full
-              ${!hasAccess ? 'cursor-not-allowed opacity-50' : ''}`}
-          >
-            <FaSearch className="text-white mx-auto" size={20} />
-          </button>
+    <>
+      <div className={`mx-auto hidden sm:block p-2 ${className || ""}`}>
+        <div className="relative">
+          <div className="flex flex-col sm:flex-row p-3 items-center bg-gray-100 rounded-3xl sm:rounded-full shadow-md overflow-hidden disabled">
+            <input
+              type="text"
+              placeholder="Where to?"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Move in:"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Move out:"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Who?"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <button
+              disabled
+              className="w-full sm:w-auto mt-3 sm:mt-0 p-3 cursor-not-allowed bg-primaryBrand rounded-full opacity-50"
+            >
+              <FaSearch className="text-white mx-auto" size={20} />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={`mx-auto block sm:hidden p-2 ${className || ""}`}>
+        <div className="relative">
+          <div className="flex flex-col sm:flex-row p-3 items-center bg-gray-100 rounded-3xl sm:rounded-full shadow-md overflow-hidden disabled">
+            <input
+              type="text"
+              placeholder="Where to?"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Move in:"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Move out:"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none sm:border-r border-gray-300 cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <input
+              type="text"
+              placeholder="Who?"
+              className="w-full sm:w-1/4 px-4 py-3 text-gray-700 placeholder-gray-400 focus:outline-none cursor-not-allowed opacity-50 bg-transparent"
+              readOnly
+            />
+            <button
+              disabled
+              className="w-full sm:w-auto mt-3 sm:mt-0 p-3 cursor-not-allowed bg-primaryBrand rounded-full opacity-50"
+            >
+              <FaSearch className="text-white mx-auto" size={20} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
