f939dfb32167c3ab7483ba1d81e57ad82bd90a26
Tyler Bennett <tyler.bennett52@gmail.com>
2024-12-10 12:42:46 -0600

    overview tab changes

--- src/app/platform/trips/[tripId]/(tabs)/overview-tab.tsx
+++ src/app/platform/trips/[tripId]/(tabs)/overview-tab.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import SearchEditBar from '@/components/home-components/search-edit-bar';
 import { MatchbookVerified } from '@/components/icons/views';
-import { RejectIcon } from '@/components/icons/actions';
+import { DeniedPaperIcon, PaperIcon, RejectIcon } from '@/components/icons/actions';
 
 const OverviewTab: React.FC = () => {
   return (
@@ -28,9 +28,9 @@ const OverviewTab: React.FC = () => {
           <div className="flex items-center gap-3">
             <div className="text-gray-600">
               {/* import paper icon */}
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13 7L11.8845 4.76892C11.5634 4.1268 10.9344 3.71429 10.2309 3.71429H5.78571C4.79949 3.71429 4 4.51378 4 5.5V18.5C4 19.4862 4.79949 20.2857 5.78571 20.2857H18.2143C19.2005 20.2857 20 19.4862 20 18.5V9.28571C20 8.29949 19.2005 7.5 18.2143 7.5L13 7Z" stroke="currentColor" strokeWidth="2"/>
-              </svg>
+              <div className="w-1/2">
+                <PaperIcon className="w-[65px] h-[65px] "/>
+              </div>
             </div>
             <span className="text-lg font-medium">Application</span>
           </div>
@@ -38,8 +38,8 @@ const OverviewTab: React.FC = () => {
 
         <div className="flex-1 p-6 border rounded-lg shadow-sm cursor-pointer hover:shadow-md transition-shadow">
           <div className="flex items-center gap-3">
-            <div className="text-[#869A7D]">
-              <MatchbookVerified />
+            <div className="text-[#869A7D] w-1/2">
+              <MatchbookVerified className="w-[65px] h-[65px]"/>
             </div>
             <span className="text-lg font-medium">Matchbook Verification</span>
           </div>
@@ -47,21 +47,19 @@ const OverviewTab: React.FC = () => {
 
         <div className="flex-1 p-6 border rounded-lg shadow-sm cursor-pointer hover:shadow-md transition-shadow">
           <div className="flex items-center gap-3">
-            <div className="text-white bg-pinkBrand rounded-full  w-[65px] h-[65px] flex items-center justify-center ">
-              X
+            <div className="w-1/2">
+              <RejectIcon className="w-[65px] h-[65px] bg-pinkBrand rounded-full p-2"/>
             </div>
-            <h3 className="text-lg font-medium ">Disliked Properties</h3>
+            <h3 className=" font-medium ">Disliked Properties</h3>
           </div>
         </div>
 
         <div className="flex-1 p-6 border rounded-lg shadow-sm cursor-pointer hover:shadow-md transition-shadow">
           <div className="flex items-center gap-3">
-            <div className="text-gray-600">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13 7L11.8845 4.76892C11.5634 4.1268 10.9344 3.71429 10.2309 3.71429H5.78571C4.79949 3.71429 4 4.51378 4 5.5V18.5C4 19.4862 4.79949 20.2857 5.78571 20.2857H18.2143C19.2005 20.2857 20 19.4862 20 18.5V9.28571C20 8.29949 19.2005 7.5 18.2143 7.5L13 7Z" stroke="currentColor" strokeWidth="2"/>
-                <path d="M15.5355 8.46447L8.46447 15.5355" stroke="#FF0000" strokeWidth="2" strokeLinecap="round"/>
-                <path d="M8.46447 8.46447L15.5355 15.5355" stroke="#FF0000" strokeWidth="2" strokeLinecap="round"/>
-              </svg>
+            <div className="text-gray-600 relative">
+              <div className="w-1/2">
+                <DeniedPaperIcon className="w-[65px] h-[65px]"/>
+              </div>
             </div>
             <span className="text-lg font-medium">Declined Applications</span>
           </div>
