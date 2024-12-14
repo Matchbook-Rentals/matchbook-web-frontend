22ddeae4a1fedbd12c06f7beff8fdd11242f6fe1
Tyler Bennett <tyler.bennett52@gmail.com>
2024-12-14 16:05:59 -0600

    remove default label sizing

--- src/components/ui/tile.tsx
+++ src/components/ui/tile.tsx
@@ -8,12 +8,12 @@ interface TileProps {
   labelClassNames?: string;
 }
 
-export default function Tile({ label, icon, className, labelClassNames}: TileProps) {
+export default function Tile({ label, icon, className, labelClassNames }: TileProps) {
   return (
     <Card className={`w-[155px] h-[175px] text-charcoal flex flex-col items-center justify-between py-2 border-[2px] border-[#E3E3E3] rounded-[30px] ${className || ''}`}>
       <CardContent className="flex flex-col items-center space-y-1 justify-between h-full w-full p-0 ">
         {icon}
-        <div className={`text-center text-[20px] pb-2 w-[98%] ${labelClassNames || ''}`}>
+        <div className={`text-center pb-2 w-[98%] ${labelClassNames || ''}`}>
           {label}
         </div>
       </CardContent>
