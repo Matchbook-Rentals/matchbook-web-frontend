15cf6b2a84956d1897933d290eb0f1483142880d
Tyler Bennett <tyler.bennett52@gmail.com>
2025-01-07 13:27:32 -0600

    Revert "try moving data fetching to page.tsx as troubleshooting step for fx memory use and wait times"
    This reverts commit 4b0395affde5ce5fe212b20f4ea92ff493420ec6.

---
+++ src/app/platform/trips/[tripId]/layout.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { TripContextProvider } from '@/contexts/trip-context-provider';
+import LoadingTabs from './LoadingTabs';
+import { pullListingsFromDb } from '@/app/actions/listings';
+import { getTripById } from '@/app/actions/trips';
+import { getUserApplication } from '@/app/actions/applications';
+
+async function TripDataWrapper({ children, params }: {
+  children: React.ReactNode,
+  params: { tripId: string }
+}) {
+  const trip = await getTripById(params.tripId, { next: { tags: [`trip-${params.tripId}`, 'user-trips'] } });
+  if (!trip) { return <p> NO TRIP FOUND </p> }
+
+  const listings = await pullListingsFromDb(trip.latitude, trip.longitude, 100);
+  const application = await getUserApplication();
+  const hasApplicationData = !!application;
+
+  //UnComment this line to force permanent render of Suspense
+  //await new Promise(() => { });
+
+  return (
+    <TripContextProvider
+      tripData={trip}
+      listingData={listings}
+      hasApplicationData={hasApplicationData}
+      application={application || null}
+    >
+      {children}
+    </TripContextProvider>
+  );
+}
+
+export default function TripLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode,
+  params: { tripId: string }
+}) {
+  return (
+    <React.Suspense fallback={<LoadingTabs />}>
+      <TripDataWrapper params={params}>
+        {children}
+      </TripDataWrapper>
+    </React.Suspense>
+  );
+}
