bf550048c224352744067e70f5d5ace2697f5015
Tyler Bennett <tyler.bennett52@gmail.com>
2025-01-21 15:43:02 -0600

    update breakpoints for matchbook grid

--- src/app/platform/searches/(tabs)/search-matchbook-tab.tsx
+++ src/app/platform/searches/(tabs)/search-matchbook-tab.tsx
@@ -84,7 +84,7 @@ export function SearchMatchbookTab() {
   return (
     <>
       {matchedListings.length > 0 && (
-        <div className="grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid justify-center space-4 gap-4 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mx-auto">
           {matchedListings.map((listing) => (
             <SearchListingCard
               key={listing.id}
