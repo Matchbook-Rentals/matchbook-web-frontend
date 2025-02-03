27a3e41c114448c592dae7c5e4e38c09e5d94c22
Tyler Bennett <tyler.bennett52@gmail.com>
2025-02-02 20:13:51 -0600

    add header and change whitespace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,10 @@ const CountdownDialog = ({ isOpen, setIsOpen, autoOpen = false }: CountdownDialo
         xOnRight
         className="
         w-[95%] sm:w-4/5 md:w-3/5 
-        h-fit py-8
-        px-6 sm:px-10 
+        h-fit pb-8 pt-0 
+        px-6 sm:px-10  
         overflow-y-scroll rounded-lg flex flex-col items-start md:max-h-[700px]   ">
+      <h1 className="text-3xl sm:text-4xl text-left  mt-8  mb-2 font-semibold">Get ready for launch!</h1>
         <BrevoIframe />
       </DialogContent>
     </Dialog>
