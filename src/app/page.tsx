087e1267e8280b864387ccdcb6b2831d408a7bf0
Tyler Bennett <tyler.bennett52@gmail.com>
2025-02-02 20:13:00 -0600

    remove countdown from subscribe dialog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,10 @@ const CountdownDialog = ({ isOpen, setIsOpen, autoOpen = false }: CountdownDialo
         xOnRight
         className="
         w-[95%] sm:w-4/5 md:w-3/5 
-        h-[70vh] sm:h-[84vh] md:h-[77vh] px-6 sm:px-10 pt-4 
+        h-fit pb-8 pt-0 
+        px-6 sm:px-10  
         overflow-y-scroll rounded-lg flex flex-col items-start md:max-h-[700px]   ">
-        <Countdown />
+      <h1 className="text-3xl sm:text-4xl text-left  mt-8  mb-2 font-semibold">Get ready for launch!</h1>
         <BrevoIframe />
       </DialogContent>
     </Dialog>
