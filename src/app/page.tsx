03c1c617d967103ff2d857c70028b2d7038f1654
Tyler Bennett <tyler.bennett52@gmail.com>
2025-02-04 03:53:17 -0600

    remove forced scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const CountdownDialog = ({ isOpen, setIsOpen, autoOpen = false }: CountdownDialo
         w-[95%] sm:w-4/5 md:w-3/5 
         h-fit pb-8 pt-0 
         px-6 sm:px-10  
-        overflow-y-scroll rounded-lg flex flex-col items-start md:max-h-[700px]   ">
+        rounded-lg flex flex-col items-start md:max-h-[700px]   ">
       <h1 className="text-3xl sm:text-4xl text-left  mt-8  mb-2 font-semibold">Get ready for launch!</h1>
         <BrevoIframe />
       </DialogContent>
