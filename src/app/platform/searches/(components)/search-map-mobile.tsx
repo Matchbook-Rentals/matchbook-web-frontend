31e51be51c9e9aa083285e53730183657d96ae8d
Tyler Bennett <tyler.bennett52@gmail.com>
2025-02-22 14:40:07 -0600

    map close button fix attempt z index

--- src/app/platform/searches/(components)/search-map-mobile.tsx
+++ src/app/platform/searches/(components)/search-map-mobile.tsx
@@ -111,10 +111,10 @@ const SearchMapMobile: React.FC<SearchMapProps> = ({
         />
       )}
 
-      <div className="fixed bottom-[13vh] left-1/2 transform -translate-x-1/2 z-10">
+      <div className="fixed bottom-[13vh] left-1/2 transform -translate-x-1/2 z-50">
         <Button
           onClick={onClose}
-          className="gap-x-2 px-5 max-w-[300px] z-10 text-[16px] font-montserrat font-medium rounded-full bg-charcoalBrand text-background"
+          className="gap-x-2 px-5 max-w-[300px] text-[16px] font-montserrat font-medium rounded-full bg-charcoalBrand text-background"
         >
           <RejectIcon className="h-5 w-5 mb-[2px]" />
           Close
