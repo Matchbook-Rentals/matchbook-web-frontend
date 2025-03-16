2a0bce2cc9ea8d4aa9f58efd1e0dcfa8c31aa28c
Tyler Bennett <tyler.bennett52@gmail.com>
2025-03-16 00:31:38 -0500

    trashcan change

--- src/app/platform/trips/(trips-components)/application-identity.tsx
+++ src/app/platform/trips/(trips-components)/application-identity.tsx
@@ -212,7 +212,7 @@ export const Identification: React.FC = () => {
                     {/* Delete button */}
                     <button
                       type="button"
-                      className="bg-red-500 text-white p-1 rounded-full text-xs"
+                      className="bg-black/10 text-black p-1.5 rounded-full text-xs"
                       onClick={(e) => {
                         e.stopPropagation();
 
@@ -234,7 +234,9 @@ export const Identification: React.FC = () => {
                       }}
                       title="Delete photo"
                     >
-                      Delete
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                      </svg>
                     </button>
                   </div>
                 </div>
