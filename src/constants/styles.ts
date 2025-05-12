7fe498de027ebd8ef3cfd40025040d13a543efcf
Tyler Bennett <tyler.bennett52@gmail.com>
2025-05-11 21:35:10 -0500

    fix close button location

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -6,7 +6,7 @@ export const APP_PAGE_MARGIN = 'w-full md:w-[90vw] lg:w-[95vw] px-2 md:px-0 mx-a
 
 
 export const TallDialogContent = 'sm:max-w-[536px] h-[90vh] sm:m-0 p-0 flex flex-col font-montserrat';
-export const TallDialogTitle = 'px-4 py-4 border-b border-gray-300 text-[20px] text-[#404040] text-center font-montserrat font-medium';
+export const TallDialogTitle = 'px-0 py-2  border-b border-gray-300 text-[20px] text-[#404040] text-center font-montserrat font-medium';
 export const TallDialogTrigger = 'flex items-center rounded-full p-2 px-4';
 export const TallDialogTriggerText = 'text-[#404040] text-center font-montserrat text-[16px] font-medium';
 
