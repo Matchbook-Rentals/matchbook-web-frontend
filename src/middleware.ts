1dac5c453351a9841c49df49885aeb94302a648c
Tyler Bennett <tyler.bennett52@gmail.com>
2025-05-13 04:48:39 -0500

    protect admin routes with clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,10 @@
 // middleware.ts
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/platform(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/platform(.*)",
+  "/admin(.*)" // Added protection for all admin routes
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) auth().protect();
