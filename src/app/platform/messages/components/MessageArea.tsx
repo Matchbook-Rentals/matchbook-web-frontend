239c25cdbd90b2051d6e2bb1db2f1bbb03e1e5a1
Tyler Bennett <tyler.bennett52@gmail.com>
2025-05-13 14:40:37 -0500

    Revert "force update of dvh values"
    This reverts commit f0a2ee6c6a309e7cfd2d53a8bd70a7a7c5ab2208.

--- src/app/platform/messages/components/MessageArea.tsx
+++ src/app/platform/messages/components/MessageArea.tsx
@@ -220,7 +220,7 @@ const MessageArea: React.FC<MessageAreaProps> = ({
 
       <div className="flex-1 w-full overflow-x-hidden">
         <ScrollArea ref={scrollAreaRef} className="h-full w-[101%] md:w-[100.7%] overflow-x-visible">
-          <div className={`py-2 px-4 min-h-full md:pb-2 ${isMobile ? 'pb-40' : ''}`}>
+          <div className={`py-2 px-4 min-h-full md:pb-2 ${isMobile ? 'pb-32' : ''}`}>
             <MessageList
               messages={messages}
               currentUserId={currentUserId}
