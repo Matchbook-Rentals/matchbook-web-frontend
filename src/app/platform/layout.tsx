a3d439fbad705aad158c921ca8219c26dca27a56
Tyler Bennett <tyler.bennett52@gmail.com>
2025-07-03 19:59:51 -0500

    Hide platform navbar on add-property route

--- src/app/platform/layout.tsx
+++ src/app/platform/layout.tsx
@@ -3,13 +3,18 @@
 
 import PlatformNavbar from '@/components/platform-components/platformNavbar'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 // Note: Terms agreement check moved to sign-in/sign-up flow
 
 export default function PlatformLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  
+  const shouldHideNavbar = pathname === '/platform/host/add-property'
+  
   return (
     <>
-      <PlatformNavbar />
+      {!shouldHideNavbar && <PlatformNavbar />}
       <div style={{ fontFamily: 'Poppins' }}>{children}</div>
     </>
   )
