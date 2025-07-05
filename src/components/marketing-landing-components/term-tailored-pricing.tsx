c9605e3532dd8336ede0f0923dae879bee136cdd
Tyler Bennett <tyler.bennett52@gmail.com>
2025-07-05 18:16:45 -0500

    Update ListYourProperty component with responsive mobile layout

--- src/components/marketing-landing-components/term-tailored-pricing.tsx
+++ src/components/marketing-landing-components/term-tailored-pricing.tsx
@@ -5,12 +5,12 @@ export const TermTailoredPricing = (): JSX.Element => {
   return (
     <section className="flex justify-center  py-16">
       <div className="flex flex-col gap-[34px] w-full xl:max-w-[1140px] 2xl:max-w-[1400px]">
-        <div className="gap-4 flex items-center">
-          <h1 className="relative flex-1 mt-[-1.00px] [font-family:'Poppins',Helvetica] font-medium text-gray-neutral900 text-[40px] tracking-[-2.00px] leading-[normal]">
+        <div className="gap-4 flex items-center flex-col md:flex-row">
+          <h1 className="relative flex-1 mt-[-1.00px] [font-family:'Poppins',Helvetica] font-medium text-gray-neutral900 text-2xl md:text-[40px] tracking-[-2.00px] leading-[normal] text-center md:text-left">
             Maximize Your Revenue with Term Tailored Pricing
           </h1>
 
-          <p className="relative flex-1 font-text-label-medium-regular font-[number:var(--text-label-medium-regular-font-weight)] text-gray-neutral500 text-[length:var(--text-label-medium-regular-font-size)] tracking-[var(--text-label-medium-regular-letter-spacing)] leading-[var(--text-label-medium-regular-line-height)] [font-style:var(--text-label-medium-regular-font-style)]">
+          <p className="relative flex-1 font-text-label-medium-regular font-[number:var(--text-label-medium-regular-font-weight)] text-gray-neutral500 text-[length:var(--text-label-medium-regular-font-size)] tracking-[var(--text-label-medium-regular-letter-spacing)] leading-[var(--text-label-medium-regular-line-height)] [font-style:var(--text-label-medium-regular-font-style)] text-center md:text-left">
             With Matchbook, you can adjust your monthly rent based on how long a
             renter stays. Offer better rates for longer terms and price shorter
             stays accordingly — all while staying competitive and increasing
@@ -21,8 +21,7 @@ export const TermTailoredPricing = (): JSX.Element => {
         <Card className="flex h-[338px] w-full border-0">
           <CardContent className="flex w-full h-full items-center justify-center p-0 bg-[linear-gradient(0deg,rgba(60,135,135,0)_0%,rgba(83,184,184,0)_94%)]">
             <div
-              className="w-full h-full bg-cover bg-center"
-              style={{ backgroundImage: "url(/marketing-images/term-tailored-pricing/1.png)" }}
+              className="w-full h-full bg-contain bg-center bg-no-repeat md:bg-[url(/marketing-images/term-tailored-pricing/1.png)] bg-[url(/marketing-images/term-tailored-pricing/2.png)]"
             />
           </CardContent>
         </Card>
