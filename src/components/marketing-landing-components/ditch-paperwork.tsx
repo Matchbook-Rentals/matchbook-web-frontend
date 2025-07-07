938b4f117ab6fcba4f8176485c12cb93cc4ef6f2
Tyler Bennett <tyler.bennett52@gmail.com>
2025-07-07 00:54:10 -0500

    fix responsive view

--- src/components/marketing-landing-components/ditch-paperwork.tsx
+++ src/components/marketing-landing-components/ditch-paperwork.tsx
@@ -15,11 +15,11 @@ export const DitchPaperwork = (): JSX.Element => {
       </Card>
 
       <div className="flex flex-col items-start gap-4 relative flex-1 grow mt-4 md:mt-0">
-        <h2 className="relative self-stretch mt-[-1.00px] font-['Poppins',Helvetica] font-medium text-gray-neutral900 text-[40px] tracking-[-2.00px] leading-[normal]">
+        <h2 className="relative self-stretch text-center md:text-left mt-[-1.00px] font-['Poppins',Helvetica] font-medium text-gray-neutral900 text-[40px] tracking-[-2.00px] leading-[normal]">
           Ditch the paperwork
         </h2>
 
-        <p className="relative self-stretch font-text-heading-small-medium font-[number:var(--text-heading-small-medium-font-weight)] text-gray-neutral500 text-[length:var(--text-heading-small-medium-font-size)] tracking-[var(--text-heading-small-medium-letter-spacing)] leading-[var(--text-heading-small-medium-line-height)] [font-style:var(--text-heading-small-medium-font-style)]">
+        <p className="relative self-stretch text-center md:text-left font-text-heading-small-medium font-[number:var(--text-heading-small-medium-font-weight)] text-gray-neutral500 text-[length:var(--text-heading-small-medium-font-size)] tracking-[var(--text-heading-small-medium-letter-spacing)] leading-[var(--text-heading-small-medium-line-height)] [font-style:var(--text-heading-small-medium-font-style)]">
           Bookings generate automatic rental agreements, streamlining the
           leasing process—no paperwork, no waiting. Your business, modernized.
         </p>
