d3e2435116214d236438aaafd794ff99b56b0f6f
Tyler Bennett <tyler.bennett52@gmail.com>
2025-07-11 20:29:05 -0500

    add query string to force favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,13 @@ export const metadata: Metadata = {
   description: "Flexible Rentals",
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png?v=2", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png?v=2", sizes: "32x32", type: "image/png" },
     ],
-    apple: "/apple-touch-icon.png",
+    apple: "/apple-touch-icon.png?v=2",
     other: [
-      { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+      { url: "/android-chrome-192x192.png?v=2", sizes: "192x192", type: "image/png" },
+      { url: "/android-chrome-512x512.png?v=2", sizes: "512x512", type: "image/png" },
     ],
   },
   manifest: "/site.webmanifest",
