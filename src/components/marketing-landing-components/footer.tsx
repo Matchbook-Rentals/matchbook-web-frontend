638bb5bacae3d0abd8abb697d8f3ef58ad34d8fb
Tyler <tyler.bennett52@gmail.com>
2026-01-27 17:31:27 -0600

    Simplify footer: remove Legal section and village image, inline 3 social icons

--- src/components/marketing-landing-components/footer.tsx
+++ src/components/marketing-landing-components/footer.tsx
@@ -1,29 +1,22 @@
 import Link from "next/link";
-import SocialLinks from "../SocialLinks";
-
-// TODO: Rework sub-desktop views. One idea is moving the social links to a new row at mobile.
+import { FacebookIcon, InstagramIcon, LinkedinIcon } from "../Icons";
 
 export default function Footer() {
 
   return (
     <footer className="border-t border-gray-200  py-6 md:py-6">
       <div className="mx-auto flex flex-col lg:flex-row justify-between w-full md:w-[90vw] lg:w-[80vw]">
-        {/* Company Logo and Tagline */}
+        {/* Company Logo */}
         <div className="flex flex-col  items-center gap-4 px-4 sm:px-6 md:px-8">
           <img className="h-6" src="/new-green-logo.png" alt="Company Logo" />
-          <img
-            className=" h-[100px]"
-            src="/village_footer.png"
-            alt="Village Footer"
-          />
         </div>
 
         {/* Links Section */}
-        <div className="flex justify-between w-full pt-3 lg:w-1/2 gap-2 lg:gap-0 ">
+        <div className="flex justify-between w-full pt-3 lg:w-3/5 gap-2 lg:gap-0 ">
           {/* Navigation Links */}
           <div className="text-lg mx-auto ">
             <h3 className="text-xl font-semibold">Navigation</h3>
-            <nav className="flex flex-col ">
+            <nav className="flex flex-col gap-1.5 mt-2">
               <Link
                 href="/#"
                 className="text-gray-500 hover:text-black"
@@ -36,7 +29,7 @@ export default function Footer() {
                 className="text-gray-500 hover:text-black"
                 prefetch={false}
               >
-                About Us
+                About
               </Link>
               <Link
                 href="/contact/#"
@@ -51,7 +44,7 @@ export default function Footer() {
           {/* Resource Links */}
           <div className="text-lg mx-auto">
             <h3 className="text-xl font-semibold">Resources</h3>
-            <nav className="flex flex-col">
+            <nav className="flex flex-col gap-1.5 mt-2">
               <Link
                 href="/articles"
                 className="text-gray-500 hover:text-black"
@@ -69,65 +62,33 @@ export default function Footer() {
             </nav>
           </div>
 
-          {/* Legal Links - hidden on small, shown on sm+ */}
-          <div className="text-lg mx-auto hidden sm:block">
-            <h3 className="text-xl font-semibold">Legal</h3>
-            <nav className="flex flex-col">
-              <Link
-                href="/terms-of-service"
-                className="text-gray-500 hover:text-black"
-                prefetch={false}
-              >
-                Terms of Service
-              </Link>
-              <Link
-                href="/privacy-policy"
-                className="text-gray-500 hover:text-black"
-                prefetch={false}
-              >
-                Privacy Policy
-              </Link>
-              <Link
-                href="/acceptable-use-policy"
-                className="text-gray-500 hover:text-black"
-                prefetch={false}
-              >
-                Acceptable Use
-              </Link>
-              <Link
-                href="/california-privacy-notice"
-                className="text-gray-500 hover:text-black"
-                prefetch={false}
-              >
-                CA Privacy Notice
-              </Link>
-              <Link
-                href="/cookie-notice"
-                className="text-gray-500 hover:text-black"
-                prefetch={false}
-              >
-                Cookie Notice
-              </Link>
-            </nav>
-          </div>
-
           {/* Social Media Links */}
           <div className="text-lg mx-auto">
             <h3 className="text-xl font-semibold">Follow Us</h3>
-            <SocialLinks className="pt-3 md:pt-6 w-[88px] md:w-auto justify-center" />
+            <div className="flex gap-2 pt-3">
+              <Link
+                href="https://www.facebook.com/share/MpdT4CSW4cp75vZh/?mibextid=LQQJ4d"
+                className="bg-secondaryBrand hover:bg-secondaryBrand/80 text-white rounded-full p-1.5 md:p-2.5 transition-colors duration-200"
+                prefetch={false}
+              >
+                <FacebookIcon className="h-4 w-4 md:h-6 md:w-6 lg:h-7 lg:w-7" />
+              </Link>
+              <Link
+                href="https://www.instagram.com/matchbookrentals?igsh=MWh4NngyZ2Q4Nnlmbw=="
+                className="bg-secondaryBrand hover:bg-secondaryBrand/80 text-white rounded-full p-1.5 md:p-2.5 transition-colors duration-200"
+                prefetch={false}
+              >
+                <InstagramIcon className="h-4 w-4 md:h-6 md:w-6 lg:h-7 lg:w-7" />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/company/matchbookrentals/"
+                className="bg-secondaryBrand hover:bg-secondaryBrand/80 text-white rounded-full p-1.5 md:p-2.5 transition-colors duration-200"
+                prefetch={false}
+              >
+                <LinkedinIcon className="h-4 w-4 md:h-6 md:w-6 lg:h-7 lg:w-7" />
+              </Link>
+            </div>
           </div>
-        </div>
-
-        {/* Legal Links - mobile only row */}
-        <div className="sm:hidden flex flex-col items-start pt-6 px-4">
-          <h3 className="text-xl font-semibold mb-2">Legal</h3>
-          <nav className="flex flex-wrap gap-x-4 gap-y-1">
-            <Link href="/terms-of-service" className="text-gray-500 hover:text-black" prefetch={false}>Terms of Service</Link>
-            <Link href="/privacy-policy" className="text-gray-500 hover:text-black" prefetch={false}>Privacy Policy</Link>
-            <Link href="/acceptable-use-policy" className="text-gray-500 hover:text-black" prefetch={false}>Acceptable Use</Link>
-            <Link href="/california-privacy-notice" className="text-gray-500 hover:text-black" prefetch={false}>CA Privacy Notice</Link>
-            <Link href="/cookie-notice" className="text-gray-500 hover:text-black" prefetch={false}>Cookie Notice</Link>
-          </nav>
         </div>
       </div>
     </footer>
