a15e46977e18a8de4ca8c24597f41867f000a6da
Tyler <tyler.bennett52@gmail.com>
2026-02-09 04:27:26 -0600

    Update Who popover padding to p-6 for consistency

--- src/components/home-components/GuestTypeCounter.tsx
+++ src/components/home-components/GuestTypeCounter.tsx
@@ -25,7 +25,7 @@ const GuestTypeCounter: React.FC<GuestTypeCounterProps> = ({ guests, setGuests }
   ];
 
   return (
-    <div className="p-4 space-y-4">
+    <div className="p-6 space-y-4">
       {guestTypes.map(({ type, label, min }) => (
         <div key={type} className="flex items-center justify-between">
           <div className="flex flex-col">
